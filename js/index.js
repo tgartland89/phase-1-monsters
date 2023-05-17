3aa1cd7460a8eb50aae41337735744a9d9eeaa5e
tgartland89 <thomas.gartland@gmail.com>
2023-05-16 20:22:17 -0600

    finished deliverables

--- js/index.js
+++ js/index.js
@@ -1,14 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
     let monsterContainer = document.getElementById("monster-container");
+    let createMonsterForm = document.getElementById("create-monster-form");
+  
     let backButton = document.getElementById("back");
     let forwardButton = document.getElementById("forward");
+    let createMonsterButton = document.querySelector("#create-monster-form button");
   
     let currentPage = 1;
   
-// Fetch monsters and update the page
+    // Fetch monsters and update the page
     function fetchMonsters(page) {
-      let url = `http://localhost:3000/monsters?page=${page}&limit=50`; // Replace with the actual API URL
-  
+      let url = `http://localhost:3000/monsters?_limit=50&_page=${page}`; 
+      
       fetch(url)
         .then(response => response.json())
         .then(data => {
@@ -37,11 +40,11 @@ document.addEventListener("DOMContentLoaded", () => {
         monsterContainer.appendChild(monsterElement);
       });
     }
-// Load initial monsters on page load
-    
+  
+    // Load initial monsters on page load
     fetchMonsters(currentPage);
   
-// Event listeners for pagination buttons
+    // Event listeners for pagination buttons
     backButton.addEventListener("click", () => {
       if (currentPage > 1) {
         currentPage--;
@@ -53,5 +56,54 @@ document.addEventListener("DOMContentLoaded", () => {
       currentPage++;
       fetchMonsters(currentPage);
     });
+  
+    createMonsterForm.addEventListener("submit", (event) => {
+      event.preventDefault(); // Prevent form submission from refreshing the page
+  
+      // Get the values from the form inputs
+      let nameInput = document.getElementById("name");
+      let ageInput = document.getElementById("age");
+      let descriptionInput = document.getElementById("description");
+  
+      let name = nameInput.value;
+      let age = ageInput.value;
+      let description = descriptionInput.value;
+  
+      // Create a new monster object with the form values
+      let newMonster = {
+        name: name,
+        age: age,
+        description: description,
+      };
+  
+      // Call a function to save the new monster
+      saveMonster(newMonster);
+    });
+  
+    function saveMonster(monster) {
+      let url = "http://localhost:3000/monsters"; // Replace with the actual API URL
+  
+      fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(monster),
+      })
+        .then(response => response.json())
+        .then(data => {
+          // Handle the response from the API
+          console.log("New monster saved:", data);
+  
+          // Clear the form inputs
+          createMonsterForm.reset();
+  
+          // Fetch and render the updated list of monsters
+          fetchMonsters(currentPage);
+        })
+        .catch(error => {
+          console.error("Error saving monster:", error);
+        });
+    }
   });
   
